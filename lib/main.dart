58c0509fdee4f0de20a6df2e44ada2fd0afe2cbd
Rocktopus101 <vedaantrajeshirke@icloud.com>
2022-12-15 16:46:29 +0530

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
@@ -31,6 +32,14 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  List<bool> answers = [false, true, true];
+  var questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -43,7 +52,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(15.0),
             child: Center(
               child: Text(
-                'This is where question text will go',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,12 +70,19 @@ class _QuizPageState extends State<QuizPage> {
                 backgroundColor: MaterialStateProperty.all(Colors.green),
               ),
               onPressed: () {
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == true) {
+                  print('User entered correct answer!');
+                } else {
+                  print('User entered wrong answer!');
+                }
                 //The user picked true.
                 setState(() {
                   scoreKeeper.add(Icon(
                     Icons.check,
                     color: Colors.green,
                   ));
+                  questionNumber = questionNumber + 1;
                 });
               },
               child: Text(
@@ -87,7 +103,16 @@ class _QuizPageState extends State<QuizPage> {
                 backgroundColor: MaterialStateProperty.all(Colors.red),
               ),
               onPressed: () {
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == false) {
+                  print('User entered correct answer!');
+                } else {
+                  print('User entered wrong answer!');
+                }
                 //The user picked false.
+                setState(() {
+                  questionNumber = questionNumber + 1;
+                });
               },
               child: Text(
                 'False',
